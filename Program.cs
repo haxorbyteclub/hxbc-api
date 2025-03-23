ae1499717288ab03e2e95935b5a24da303f0d887
JL <nor0x@users.noreply.github.com>
2025-03-23 21:27:17 +0100

    as per 1a4eaf9a2c54ffb9c05f1c6edb2c10bd5df0029b client

--- Program.cs
+++ Program.cs
@@ -81,5 +81,13 @@ app.MapGet("/api/v0/hackernews", async () =>
     return Results.Content(response, "application/rss+xml");
 });
 
+
+app.MapGet("/api/v0/music", async () =>
+{
+    var musicFilePath = Path.Combine(app.Environment.ContentRootPath, "data", "music.json");
+    var json = await File.ReadAllTextAsync(musicFilePath);
+    return Results.Content(json, "application/json");
+})
+
 app.Run();
 
